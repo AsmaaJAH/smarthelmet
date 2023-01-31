762bca3aeb0e6de64baccd68edf348adad519f8c
Salah-Eltenehy <91028700+Salah-Eltenehy@users.noreply.github.com>
2023-01-31 22:44:15 +0200

    finish sign up screen

--- lib/modules/signup/SignUp.dart
+++ lib/modules/signup/SignUp.dart
@@ -7,69 +7,132 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   var usernameController = TextEditingController();
-
   var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var phoneController =  TextEditingController();
   var prefixIcon = Icons.lock;
   var suffexIcon = Icons.visibility_off;
   var secure = true;
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            Center(
-              child: CircleAvatar(
-                radius: 60,
-                child: Image(
-                  image: AssetImage('assets/images/helmet.jpeg'),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 60,
+              ),
+              Center(
+                child: CircleAvatar(
+                  radius: 60,
+                  child: Image(
+                    image: AssetImage('assets/images/helmet.jpeg'),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            defaultTextFormFieldColumn(
-                controller: usernameController,
-                validatorFunction: (value) {
-                  if (value.length == 0) return 'this field is requreid';
+              SizedBox(
+                height: 10,
+              ),
+              defaultTextFormFieldColumn(
+                  controller: usernameController,
+                  validatorFunction: (value) {
+                    if (value.length == 0) return 'this field is requreid';
+                  },
+                  textInputType: TextInputType.text,
+                  labelText: 'User name'),
+              SizedBox(
+                height: 10,
+              ),
+              defaultTextFormFieldColumn(
+                  controller: emailController,
+                  validatorFunction: (value) {
+                    if (value.length == 0) return 'this field is requreid';
+                  },
+                  textInputType: TextInputType.emailAddress,
+                  labelText: 'Email address',
+                  prefixIcon: Icon(Icons.email)),
+              SizedBox(
+                height: 10,
+              ),
+              defaultTextFormFieldColumn(
+                  controller: passwordController,
+                  validatorFunction: (value) {
+                    if (value.length == 0) return 'this field is requreid';
+                    if (value.length < 8) return 'password is too short';
+                  },
+                  textInputType: TextInputType.visiblePassword,
+                  labelText: 'Password',
+                  prefixIcon: Icon(prefixIcon),
+                  isSecure: secure,
+                  suffixIcon: suffexIcon,
+                  suffixIconFunction: () {
+                    secure = !secure;
+                    prefixIcon = secure ? Icons.lock : Icons.lock_open;
+                    suffexIcon = secure ? Icons.visibility_off: Icons.visibility;
+                    setState(() {});
+                  }),
+              SizedBox(
+                height: 10,
+              ),
+              defaultTextFormFieldColumn(
+                  controller: phoneController,
+                  validatorFunction: (value) {
+                    if (value.length == 0) return 'this field is requreid';
+                    else if (value.length != 13) return 'phone is in valid';
+                  },
+                  textInputType: TextInputType.number,
+                  labelText: 'Phone number',
+                  prefixIcon: Icon(Icons.phone),
+                  ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  const Text('Have an account? '),
+                  TextButton(
+                      onPressed: () {
+
+                      },
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(
+                          color: Colors.blue
+                        ),
+                      )
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              InkWell(
+                onTap: () {
+                    if(formKey.currentState!.validate()) {
+                      print("All done");
+                    }
                 },
-                textInputType: TextInputType.text,
-                labelText: 'User name'),
-            SizedBox(
-              height: 10,
-            ),
-            defaultTextFormFieldColumn(
-                controller: emailController,
-                validatorFunction: (value) {
-                  if (value.length == 0) return 'this field is requreid';
-                },
-                textInputType: TextInputType.emailAddress,
-                labelText: 'Email address',
-                prefixIcon: Icon(Icons.email)),
-            SizedBox(
-              height: 10,
-            ),
-            defaultTextFormFieldColumn(
-                controller: emailController,
-                validatorFunction: (value) {
-                  if (value.length == 0) return 'this field is requreid';
-                },
-                textInputType: TextInputType.visiblePassword,
-                labelText: 'Password',
-                prefixIcon: Icon(prefixIcon),
-                isSecure: secure,
-                suffixIcon: suffexIcon,
-                suffixIconFunction: () {
-                  secure = !secure;
-                  prefixIcon = secure ? Icons.lock : Icons.lock_open;
-                  
-                }),
-          ],
+                child: Container(
+                  height: 40,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue
+                  ),
+                  child:const  Center(child: Text('Create an acount'
+                  ,style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
