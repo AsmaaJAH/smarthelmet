e482ef1a170712cf88c1b6ba9d6e0074b23a448f
Salah-Eltenehy <91028700+Salah-Eltenehy@users.noreply.github.com>
2023-02-01 23:39:24 +0200

    fix firebase problem

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,14 @@ import 'package:smarthelmet/modules/signup/SignUp.dart';
 // ignore_for_file: prefer_equal_for_default_values
 import 'package:firebase_core/firebase_core.dart';
 import 'package:smarthelmet/shared/network/local/cache_helper.dart';
-
+import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CachHelper.init();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   var userID = await CachHelper.getData(key: "uid");
   Widget startWidget = SignInScreen();
   if (userID != null) {
