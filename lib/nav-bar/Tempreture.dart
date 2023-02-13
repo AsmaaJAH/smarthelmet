a6d89b377983549da5f82b71eaa5e667025df3c7
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-02-13 19:45:41 +0200

    tempreture in the nav bar

--- lib/nav-bar/Tempreture.dart
+++ lib/nav-bar/Tempreture.dart
@@ -1,13 +1,90 @@
 
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:smarthelmet/shared/constants/Constants.dart';
 
-class TempretureScreen extends StatelessWidget {
+import '../shared/functions/CircleProgress.dart';
+
+class TempretureScreen extends StatefulWidget {
+
+  @override
+  State<TempretureScreen> createState() => _TempretureScreenState();
+}
+
+class _TempretureScreenState extends State<TempretureScreen> with TickerProviderStateMixin {
+
+  late Animation<double> tempAnimation;
+  late AnimationController progressController;
+
+  @override
+  void initState() {
+    super.initState();
+ double temp = 20;
+    //temp = sensorsTable['temp'] as double;
+    double humdity = 100;
+    //humidity = sensorsTable['Humdity'] as double;
+
+    _FetchDataInit(temp, humdity);
+   
+  }
+
+   _FetchDataInit(double temp, double humid) {
+    progressController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 5000)); //5s
+
+    tempAnimation =
+        Tween<double>(begin: -50, end: temp).animate(progressController)
+          ..addListener(() {
+            setState(() {});
+          });
+
+    progressController.forward();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      body: Column(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CustomPaint(
+                    foregroundPainter:
+                        CircleProgress(tempAnimation.value, true),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('Temperature'),
+                            Text(
+                              '${tempAnimation.value.toInt()}',
+                              style: TextStyle(
+                                  fontSize: 50, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '°C',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+
+              ),
+            ),
+          ],
+      ),
+    
+    appBar: AppBar(
         title: const Text(
           'Temperature',
           style: TextStyle(
