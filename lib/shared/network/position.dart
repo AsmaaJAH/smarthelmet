fddf61b494c4767b192db48d55afbd714c06c11d
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-03-17 13:01:59 +0200

    tracking positions errors

--- lib/shared/network/position.dart
+++ lib/shared/network/position.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+import 'dart:developer';
+
 import 'package:firebase_database/firebase_database.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class position {
   double latitude1;
   double longitude1;
@@ -9,7 +14,8 @@ final dataBase = FirebaseDatabase.instance.ref();
 Map<Object?, Object?> gpsTable = {};
 Map<String, List<String>> table = {
   "gps": [
-    'latitude1', 'longitude1',
+    'latitude1',
+    'longitude1',
   ],
 };
 void readRealTimeDatabase() async {
@@ -18,16 +24,24 @@ void readRealTimeDatabase() async {
     await dbRef.onValue.listen((event) {
       if (key == "gps") {
         gpsTable = event.snapshot.value as Map<Object?, Object?>;
-          positions.add(new position(
-              latitude1:
-                  double.tryParse('${gpsTable['latitude1']}' ?? '') ?? 0.0,
-              longitude1:
-                  double.tryParse('${gpsTable['longitude1']}' ?? '') ?? 0.0)); 
+        positions.add(new position(
+            latitude1: double.tryParse('${gpsTable['latitude1']}' ?? '') ?? 0.0,
+            longitude1:
+                double.tryParse('${gpsTable['longitude1']}' ?? '') ?? 0.0));
+        positions[1] = position(
+            latitude1: double.tryParse('${gpsTable['latitude1']}' ?? '') ?? 0.0,
+            longitude1:
+                double.tryParse('${gpsTable['longitude1']}' ?? '') ?? 0.0);
+        print(positions[1].toString());
       }
     });
   });
 }
+
 List<position> positions = [
-  //position(latitude1: double.tryParse('${gpsTable['latitude1']}' ?? '') ?? 0.0    , longitude1: double.tryParse('${gpsTable['longitude1']}' ?? '') ?? 0.0 ),
+  position(
+      latitude1: double.tryParse('${gpsTable['latitude1']}' ?? '') ?? 0.0,
+      longitude1: double.tryParse('${gpsTable['longitude1']}' ?? '') ?? 0.0),
   // position(latitude1: 0.0    , longitude1: 0.0 ),
 ];
+List<LatLng> pos = [];
