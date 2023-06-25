6ad7878a468f1002920ad836a1ffc13f12f54860
Asmaa Gamal <88660261+AsmaaJAH@users.noreply.github.com>
2023-06-25 21:53:56 +0300

    logo + normal for remaining workers data alerts

--- lib/shared/screens/Alert_info.dart
+++ lib/shared/screens/Alert_info.dart
@@ -1,13 +1,15 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class AlertInfo extends StatefulWidget {
   String data;
   String alertname;
   double fontsize;
-
+  late AsyncSnapshot<dynamic> snapshot;
+  late int index;
   AlertInfo(
-      {required this.data, required this.alertname, required this.fontsize});
+      {required this.data, required this.alertname, required this.fontsize, required this.snapshot, required this.index});
 
   @override
   State<AlertInfo> createState() => _AlertInfoState();
@@ -15,7 +17,53 @@ class AlertInfo extends StatefulWidget {
 
 class _AlertInfoState extends State<AlertInfo> {
   var Group = AutoSizeGroup();
+  final dataBase = FirebaseDatabase.instance.ref();
 
+  Map<Object?, Object?> alertTable = {};
+
+  void showNormal() async {
+    tables.forEach((key, value) async {
+      Query dbRef = FirebaseDatabase.instance.ref().child(key);
+      await dbRef.onValue.listen((event) {
+        print(event.snapshot.value);
+        if (key == "ALERT") {
+          alertTable = event.snapshot.value as Map<Object?, Object?>;
+        }
+        setState(() {});
+      });
+    });
+  }
+
+  Map<String, List<String>> tables = {
+    "ALERT": [
+      'HUM',
+      'LPG',
+      'CO',
+      'TEMP',
+      'fall',
+      'object',
+      'uid',
+      'medicalAssistance'
+    ],
+    "sensors": [
+      'CO PPM value',
+      'Humdity',
+      'LPG PPM value',
+      'temp',
+      'undergroundX',
+      'undergroundY',
+    ],
+    "gps": [
+      'latitude1',
+      'longitude1',
+    ],
+  };
+
+  @override
+  void initState() {
+    showNormal();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -29,7 +77,7 @@ class _AlertInfoState extends State<AlertInfo> {
             style: TextStyle(
                 fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
           ),
-          widget.data == 'normal'
+        widget.data == 'normal' 
               ? Expanded(
                   child: AutoSizeText(
                     "Normal",
@@ -41,7 +89,8 @@ class _AlertInfoState extends State<AlertInfo> {
                         color: Colors.green),
                   ),
                 )
-              : Expanded(
+              :  "${widget.snapshot.data!.docs[widget.index]["uid"]}" == alertTable['uid'].toString()?
+              Expanded(
                   child: AutoSizeText(
                     widget.data,
                     maxLines: 2,
@@ -53,7 +102,20 @@ class _AlertInfoState extends State<AlertInfo> {
                         fontWeight: FontWeight.bold),
                     overflowReplacement: Text('Sorry String too long'),
                   ),
-                ),
+                )
+                :
+                Expanded(
+                  child: AutoSizeText(
+                    "Normal",
+                    maxLines: 2,
+                    group: Group,
+                    style: TextStyle(
+                        fontSize: widget.fontsize,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green),
+                  ),
+                )
+
         ],
       ),
     );
